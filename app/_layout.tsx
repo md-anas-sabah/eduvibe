327cf2c51e37c4de8b453ec9bf02ac8a40291422
md-anas-sabah <mdanassabah@gmail.com>
2025-02-13 22:39:51 +0530

    feat: added types

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { Stack } from "expo-router";
-import index from ".";
 import { ThemeProvider } from "@/context/theme.context";
 
 export default function _layout() {
@@ -13,6 +11,7 @@ export default function _layout() {
         }}
       >
         <Stack.Screen name="index" />
+        <Stack.Screen name="(routes)/onboarding/index" />
       </Stack>
     </ThemeProvider>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,29 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
+import { Redirect } from "expo-router";
 
 export default function index() {
+  const [loggedInUser, setLoggedInUser] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    const subscription = async () => {
+      const token = SecureStore.getItem("token");
+      setLoggedInUser(token ? true : false);
+      setLoading(false);
+    };
+
+    subscription();
+  }, []);
+
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <>
+      {loading ? (
+        <></>
+      ) : (
+        <Redirect href={!loggedInUser ? "/(routes)/onboarding" : "/(tabs)"} />
+      )}
+    </>
   );
 }
